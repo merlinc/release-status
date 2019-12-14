97bff9233032f891ad503cc30f2ef8db416f775f
Robin Harrison <merlinc@courts.io>
2019-12-14 14:18:44 +0000

    Checkpoint broken

--- components/commit-ticket.jsx
+++ components/commit-ticket.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Ticket from "./ticket";
 import Team from "./team";
+import styled from "styled-components";
+
+const TicketWrapper = styled.div``;
 
 class CommitTicket extends Ticket {
   render() {
     let title = this.props.ticket.title + "\n\n" + this.commits().join("\n\n");
 
     return (
+      // <TicketWrapper>
       <div className="ticket nojira" style={this.buildStyle()} title={title}>
-        <span>⚐ {this.props.ticket.title}</span>{ this.props.team && <Team team={this.props.team}/> }
+        <span>⚐ {this.props.ticket.title}</span>
+        {this.props.team && <Team team={this.props.team} />}
       </div>
+      // </TicketWrapper>
     );
   }
 }
--- components/promotion.jsx
+++ components/promotion.jsx
@@ -1,5 +1,55 @@
 import React from "react";
 const utils = require("../lib/release-status-utils");
+import styled from "styled-components";
+
+const EnvWrapper = styled.div`
+  display: inline;
+  float: right;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 1px 5px 1px 5px;
+  border: ${props => {
+      if (props.envName === "promote") {
+        return "dotted";
+      } else if (props.envName === "rough") {
+        return "dashed";
+      }
+
+      return "solid";
+    }}
+    2px;
+  margin-top: -3px;
+  margin-left: -3px;
+  margin-right: 5px;
+`;
+
+const PromoteWrapper = styled(EnvWrapper)``;
+
+const RoughWrapper = styled(EnvWrapper)``;
+
+const CIWrapper = styled(EnvWrapper)`
+  color: #00d;
+  border-color: #00d;
+`;
+
+const colors = {
+  ci: "#00d"
+};
+
+const IntegrationWrapper = styled(EnvWrapper)`
+  color: #090;
+  border-color: #090;
+`;
+
+const StagingWrapper = styled(EnvWrapper)`
+  color: #990;
+  border-color: #990;
+`;
+
+const ProductionWrapper = styled(EnvWrapper)`
+  color: #d00;
+  border-color: #d00;
+`;
 
 class Promotion extends React.Component {
   render() {
