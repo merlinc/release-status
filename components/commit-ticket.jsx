5fafe64820d175af5193dd36a4140df5dc3807e2
Robin Harrison <merlinc@courts.io>
2019-12-14 17:09:26 +0000

    WIP - Tickets, MergeTickets, JiraTickets, CommitTickets

--- components/commit-ticket.jsx
+++ components/commit-ticket.jsx
@@ -3,19 +3,48 @@ import Ticket from "./ticket";
 import Team from "./team";
 import styled from "styled-components";
 
-const TicketWrapper = styled.div``;
+const colors = {
+  unknown: "rgba(255, 0, 0, 0.7)",
+  nojira: "rgba(220, 255, 255, 0.7)",
+  ready: "rgba(200, 255, 200, 0.7)",
+  develop: "rgba(255, 170, 180, 0.7)",
+  test: "rgba(190, 200, 255, 0.7)",
+  review: "rgba(220, 190, 255, 0.7)",
+  closed: "rgba(220, 220, 220, 0.7)"
+};
+
+const TicketWrapper = styled.div`
+  position: absolute;
+  padding: 5px;
+  border: solid 3px #444;
+  width: 160px;
+  overflow: hidden;
+  background-color: rgba(255, 0, 0, 0.7);
+  color: black;
+`;
+
+const CommitTicketWrapper = styled(TicketWrapper)`
+  background-color: ${props => {
+    console.log(props.ticketType);
+    return colors[props.ticketType];
+  }};
+  color: ${props => (props.ticketType === "unknown" ? "white" : "black")};
+  float: right;
+`;
 
 class CommitTicket extends Ticket {
   render() {
     let title = this.props.ticket.title + "\n\n" + this.commits().join("\n\n");
 
     return (
-      // <TicketWrapper>
-      <div className="ticket nojira" style={this.buildStyle()} title={title}>
+      <CommitTicketWrapper
+        style={this.buildStyle()}
+        title={title}
+        ticketType="nojira"
+      >
         <span>⚐ {this.props.ticket.title}</span>
         {this.props.team && <Team team={this.props.team} />}
-      </div>
-      // </TicketWrapper>
+      </CommitTicketWrapper>
     );
   }
 }
