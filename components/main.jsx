c85dc4852cc20ad423b90402f89391370b114436
Robin Harrison <merlinc@courts.io>
2020-02-18 12:44:44 +0000

    Refactored to use React Hooks
    - Replace recompose with useQuery
    - Refactor React.Component into function components
    - Add eslint-plugin-react-hooks
    - Add next-with-apollo

--- components/main.jsx
+++ components/main.jsx
@@ -1,11 +1,12 @@
-const { graphql, Query } = require("react-apollo");
-const { compose } = require("recompose");
-import gql from "graphql-tag";
-
 import React from "react";
+import gql from "graphql-tag";
 import StatusGrid from "./status-grid";
 import BreadCrumbs from "./bread-crumbs";
-export const projectStatus = gql`
+import withApollo from "../lib/withApolloNew";
+import { useQuery } from "@apollo/react-hooks";
+import { getDataFromTree } from "@apollo/react-ssr";
+
+export const QUERY = gql`
   query status($org: String!, $project: String!) {
     status(org: $org, project: $project) {
       project
@@ -33,69 +34,28 @@ export const projectStatus = gql`
   }
 `;
 
-class Main extends React.Component {
-  render() {
-    return (
-      <Query
-        query={projectStatus}
-        variables={{ org: this.props.org, project: this.props.project }}
-      >
-        {({ loading, error, data }) => {
-          if (loading) return "Loading...";
-          if (error) return `Error! ${error.message}`;
+const Main = (props) => {
+  const { loading, error, data } = useQuery(QUERY, {
+    variables: { org: props.org, project: props.project }
+  });
 
-          return (
-            <div>
-              <BreadCrumbs org={this.props.org} project={this.props.project} />
-              <StatusGrid
-                commits={data.status.commits}
-                tickets={this.props.showTickets && data.status.tickets}
-              />
+  if (loading) return "Loading...";
+  if (error) return "Error...";
 
-              {/*<StatusGrid commits={this.props.status.commits} tickets={this.props.status.tickets} releases={this.props.status.commits} updates={}/>*/}
+  return (
+    <div>
+      <BreadCrumbs org={props.org} project={props.project} />
+      <StatusGrid
+        commits={data.status.commits}
+        tickets={props.showTickets && data.status.tickets}
+      />
+      {/*tickets={this.props.showTickets && data.status.tickets}*/}
 
-              {/* <MoreLink options={this.props.options} /> */}
-            </div>
-          );
-        }}
-      </Query>
-    );
-  }
-}
+      {/*<StatusGrid commits={this.props.status.commits} tickets={this.props.status.tickets} releases={this.props.status.commits} updates={}/>*/}
 
-export default Main;
-// export const projectConfig = gql`
-//   query config($org: String!, $project: String!) {
-//     config(org: $org, project: $project) {
-//       org
-//       project
-//       type
-//       git {
-//         baseUrl
-//         commitUrl
-//         compareUrl
-//       }
-//     }
-//   }
-// `;
+      {/* <MoreLink options={this.props.options} /> */}
+    </div>
+  );
+};
 
-// // The `graphql` wrapper executes a GraphQL query and makes the results
-// // available on the `data` prop of the wrapped component (PostList)
-// export default compose(
-//   graphql(projectStatus, {
-//     options: props => ({
-//       variables: { org: props.org, project: props.project }
-//     }),
-//     props: ({ data }) => {
-//       return data;
-//     }
-//   }),
-//   // graphql(projectConfig, {
-//   //   options: props => ({
-//   //     variables: { org: props.org, project: props.project }
-//   //   }),
-//   //   props: ({ data }) => {
-//   //     return data;
-//   //   }
-//   // })
-// )(Main);
+export default withApollo(Main, { getDataFromTree });
